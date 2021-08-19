ad3cd6e0e582fff1ce8e890963035f37e5f4ecb6
Salinak <kansakarsalina@gmail.com>
2021-08-20 00:15:40 +0545

    Add on slick slider and done with main banner responsiveness

---
+++ assets/js/custom.js
@@ -0,0 +1,42 @@
+$(document).ready(function() { 
+    // Beneficiaries homepage slick slider
+    $('.beneficiaries__slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        variableWidth: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+
+    // Gallery homepage slick slider
+    $('.gallery__slider').slick({
+        dots: false,
+        arrows: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        adaptiveHeight: true,
+        responsive: [
+            {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: false
+            }
+        },
+            {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                variableWidth: true,
+                infinite: true
+            }
+        }
+        ]
+    });
+});
